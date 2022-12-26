616936134c58fc85113c82375ea2c0b65051150d
C <jswlds2@gmail.com>
2022-12-26 14:51:34 +0900

    Changes

--- src/C_2022_12_26/InsertionSort.java
+++ src/C_2022_12_26/InsertionSort.java
@@ -20,6 +20,9 @@ public class InsertionSort {
     public static void main(String[] args) {
         int [] arr = {3, 1, 2, 6, 7, 5, 4};
         inserSort(arr);
+//        for(int i=0; i<arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
         System.out.println(Arrays.toString(arr));
     }
 }
