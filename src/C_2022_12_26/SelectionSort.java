08162e0f302608767ee4294f90aeb6ed0ab16dfe
Jeong-seo-won <94349879+jswlds@users.noreply.github.com>
2022-12-26 14:05:37 +0900

    Changes

---
+++ src/C_2022_12_26/SelectionSort.java
@@ -0,0 +1,21 @@
+package C_2022_12_26;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int [] arr = {5, 4, 1, 2, 4};
+
+        for(int i=0; i<arr.length; i++){
+            for(int j= i+1; j<arr.length; j++){
+
+                if(arr[i]> arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
