5268e7289f37c7017661cd628479669351db282f
C <jswlds2@gmail.com>
2022-12-26 15:18:22 +0900

    Changes

---
+++ src/C_2022_12_26/BubbleSort.java
@@ -0,0 +1,24 @@
+package C_2022_12_26;
+
+public class BubbleSort {
+     public static void bubbleSort(int [] arr) {
+
+         for(int i=1; i<=arr.length-1; i++){
+             for(int j=0; j<arr.length-i; j++){
+                 if(arr[j]>arr[j+1]){
+                     int temp = arr[j];
+                     arr[j]=arr[j+1];
+                     arr[j+1]=temp;
+                 }
+             }
+         }
+     }
+
+    public static void main(String[] args) {
+        int [] arr = new int []{16, 42, 32, 25, 10, 4, 45, 9};
+        bubbleSort(arr);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
