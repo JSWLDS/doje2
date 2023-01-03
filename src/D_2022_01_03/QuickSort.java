82da393212313ed9cb1277f22cbe143ff0c4305c
Administrator <jswlds2@gmail.com>
2023-01-03 14:04:51 +0900

    Changes

--- src/D_2022_01_03/QuickSort.java
+++ src/D_2022_01_03/QuickSort.java
@@ -9,7 +9,7 @@ public class QuickSort {
     public static void quickSort(int[] arr) {
         quickSort(arr,0,arr.length-1);
         for(int i=0; i<arr.length; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 
